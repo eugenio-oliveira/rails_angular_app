a291807d7f2f0b70704d1f067cf3e3256f9366e0
Eugênio Oliveira <eugenio@ideiasti.com.br>
2019-03-08 17:32:34 -0300

    configuration for angular

--- bookstore/src/app/app.component.ts
+++ bookstore/src/app/app.component.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css'],
+providers: [HttpClient]
 })
 export class AppComponent {
-  title = 'My Angular/Rails BookStore';
+title = 'My Angular/Rails App!';
+books;
 
-  books;
-
-  constructor(private http: Http) {
-    http.get('http://localhost:3000/books.json').subscribe(res => this.books = res.json());
-  }
+constructor(private http:HttpClient){
+this.http.get('http://localhost:3000/books.json')
+.subscribe(res => this.books = res);
 }
-
-
+}
